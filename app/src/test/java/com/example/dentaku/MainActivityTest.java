7184135cfd46f498630a1b8cddc9b5b1937d1aaa
ha-akimoto <nagiha0000@yahoo.co.jp>
2020-02-17 15:52:16 +0900

    負の値の入力は受け付けていないので、 負の値の入力に関するテストを削除

--- app/src/test/java/com/example/dentaku/MainActivityTest.java
+++ app/src/test/java/com/example/dentaku/MainActivityTest.java
@@ -43,7 +43,7 @@ public class MainActivityTest {
             activity.sb = input;
         }
         activity.x = x;
-        
+
         activity.buttonEquals = new Button(this.context);
         activity.buttonPlus = new Button(this.context);
         activity.buttonMinus = new Button(this.context);
@@ -95,10 +95,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 足し算の計算で、入力値が正の値で小数点以下の値が無いの場合
+     * 足し算の計算で、入力値に小数点以下の値が無い場合
      * 計算処理が行われ、小数点以上の値がViewに設定されること
      * 演算フラグ：プラス
-     * 入力値：正の値、小数点以下なし
+     * 入力値：小数点以下なし
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
      */
     @Test
@@ -126,10 +126,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 足し算の計算で、入力値が正の値で小数点以下の値が有る場合
+     * 足し算の計算で、入力値に小数点以下の値が有る場合
      * 計算処理が行われ、小数点以下の値も含めた値がViewに設定されること
      * 演算フラグ：プラス
-     * 入力値：正の値、小数点以下有り
+     * 入力値：小数点以下有り
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
      */
     @Test
@@ -157,72 +157,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 足し算の計算で、入力値が負の値で小数点以下の値が無いの場合
-     * 計算処理が行われ、小数点以上の値がViewに設定されること
-     * 演算フラグ：プラス
-     * 入力値：負の値、小数点以下なし
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
-     */
-    @Test
-    public void plusNegative() {
-        // 演算子：足し算
-        int operator = this.FLG_PLUS;
-        // 入力値X
-        int x = -240;
-        // 入力値Y
-        int y = -327;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_PLUS);
-        input.append(y);
-        // 期待値
-        String result = Integer.toString(x + y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 足し算の計算で、入力値が負の値で小数点以下の値が有る場合
-     * 計算処理が行われ、小数点以下を含めた値がViewに設定されること
-     * 演算フラグ：プラス
-     * 入力値：負の値、小数点以下有り
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
-     */
-    @Test
-    public void plusNegativeDecimal() {
-        // 演算子：足し算
-        int operator = this.FLG_PLUS;
-        // 入力値X
-        float x = -45.234f;
-        // 入力値Y
-        float y = -02.58f;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_PLUS);
-        input.append(y);
-        // 期待値
-        String result = Float.toString(x + y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 引き算の計算で、入力値が正の値で小数点以下の値が無いの場合
+     * 引き算の計算で、入力値に小数点以下の値が無い場合
      * 計算処理が行われ、小数点以上の値がViewに設定されること
      * 演算フラグ：マイナス
-     * 入力値：正の値、小数点以下なし
+     * 入力値：小数点以下なし
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
      */
     @Test
@@ -250,10 +188,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 引き算の計算で、入力値が正の値で小数点以下の値が有る場合
+     * 引き算の計算で、入力値に小数点以下の値が有る場合
      * 計算処理が行われ、小数点以下の値も含めた値がViewに設定されること
      * 演算フラグ：マイナス
-     * 入力値：正の値、小数点以下有り
+     * 入力値：小数点以下有り
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
      */
     @Test
@@ -281,51 +219,20 @@ public class MainActivityTest {
     }
 
     /**
-     * 引き算の計算で、入力値が負の値で小数点以下の値が無いの場合
-     * 計算処理が行われ、小数点以上の値がViewに設定されること
+     * 引き算の計算で、入力値に小数点以下の値が有り、計算結果が負の値になる場合
+     * 計算処理が行われ、小数点以下の値も含めた値がViewに設定されること
      * 演算フラグ：マイナス
-     * 入力値：負の値、小数点以下なし
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
-     */
-    @Test
-    public void minusNegative() {
-        // 演算子：引き算
-        int operator = this.FLG_MINUS;
-        // 入力値X
-        int x = -294;
-        // 入力値Y
-        int y = -301;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_MINUS);
-        input.append(y);
-        // 期待値
-        String result = Integer.toString(x - y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 引き算の計算で、入力値が負の値で小数点以下の値が有る場合
-     * 計算処理が行われ、小数点以下を含めた値がViewに設定されること
-     * 演算フラグ：マイナス
-     * 入力値：負の値、小数点以下あり
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
+     * 入力値：小数点以下有り
+     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り、負の値）
      */
     @Test
     public void minusNegativeDecimal() {
         // 演算子：引き算
         int operator = this.FLG_MINUS;
         // 入力値X
-        float x = -402.4f;
+        float x = 2.24f;
         // 入力値Y
-        float y = -968.234f;
+        float y = 340.23f;
         // 入力値の文字列
         StringBuilder input = new StringBuilder();
         input.append(x);
@@ -343,10 +250,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 掛け算の計算で、入力値が正の値で小数点以下の値が無いの場合
+     * 掛け算の計算で、入力値に小数点以下の値が無い場合
      * 計算処理が行われ、小数点以上の値がViewに設定されること
      * 演算フラグ：かける
-     * 入力値：正の値、小数点以下なし
+     * 入力値：小数点以下なし
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
      */
     @Test
@@ -374,10 +281,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 掛け算の計算で、入力値が正の値で小数点以下の値が有る場合
+     * 掛け算の計算で、入力値に小数点以下の値が有る場合
      * 計算処理が行われ、小数点以下の値も含めた値がViewに設定されること
      * 演算フラグ：かける
-     * 入力値：正の値、小数点以下有り
+     * 入力値：小数点以下有り
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
      */
     @Test
@@ -405,72 +312,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 掛け算の計算で、入力値が負の値で小数点以下の値が無いの場合
-     * 計算処理が行われ、小数点以上の値がViewに設定されること
-     * 演算フラグ：かける
-     * 入力値：負の値、小数点以下なし
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
-     */
-    @Test
-    public void timesNegative() {
-        // 演算子：掛け算
-        int operator = this.FLG_TIMES;
-        // 入力値X
-        int x = -396;
-        // 入力値Y
-        int y = -950;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_TIMES);
-        input.append(y);
-        // 期待値
-        String result = Integer.toString(x * y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 掛け算の計算で、入力値が負の値で小数点以下の値が有るの場合
-     * 計算処理が行われ、小数点以下も含めた値がViewに設定されること
-     * 演算フラグ：かける
-     * 入力値：負の値、小数点以下有り
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
-     */
-    @Test
-    public void timesNegativeDecimal() {
-        // 演算子：掛け算
-        int operator = this.FLG_TIMES;
-        // 入力値X
-        float x = -3.625f;
-        // 入力値Y
-        float y = -92.598f;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_TIMES);
-        input.append(y);
-        // 期待値
-        String result = Float.toString(x * y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 割り算の計算で、入力値が正の値で小数点以下の値が無いの場合
+     * 割り算の計算で、入力値に小数点以下の値が無いの場合
      * 計算処理が行われ、小数点以上の値がViewに設定されること
      * 演算フラグ：割る
-     * 入力値：正の値、小数点以下なし
+     * 入力値：小数点以下なし
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
      */
     @Test
@@ -498,10 +343,10 @@ public class MainActivityTest {
     }
 
     /**
-     * 割り算の計算で、入力値が正の値で小数点以下の値が有る場合
+     * 割り算の計算で、入力値に小数点以下の値が有る場合
      * 計算処理が行われ、小数点以下の値も含めた値がViewに設定されること
      * 演算フラグ：割る
-     * 入力値：正の値、小数点以下有り
+     * 入力値：小数点以下有り
      * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
      */
     @Test
@@ -527,68 +372,5 @@ public class MainActivityTest {
         activity.equals(view);
         Assert.assertEquals(result, view.getText());
     }
-
-    /**
-     * 割り算の計算で、入力値が負の値で小数点以下の値が無いの場合
-     * 計算処理が行われ、小数点以上の値がViewに設定されること
-     * 演算フラグ：割る
-     * 入力値：負の値、小数点以下なし
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下なし）
-     */
-    @Test
-    public void dividedNegative() {
-        // 演算子：割り算
-        int operator = this.FLG_DIVIDED;
-        // 入力値X
-        int x = -392;
-        // 入力値Y
-        int y = -2;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_DIVIDED);
-        input.append(y);
-        // 期待値
-        String result = Integer.toString(x / y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
-    /**
-     * 割り算の計算で、入力値が負の値で小数点以下の値が有る場合
-     * 計算処理が行われ、小数点以下を含めた値がViewに設定されること
-     * 演算フラグ：割る
-     * 入力値：負の値、小数点以下有り
-     * 期待値：Viewに設定されるテキスト=計算結果の値（小数点以下有り）
-     */
-    @Test
-    public void dividedNegativeDecimal() {
-        // 演算子：割り算
-        int operator = this.FLG_DIVIDED;
-        // 入力値X
-        float x = -582.42f;
-        // 入力値Y
-        float y = -381.39f;
-        // 入力値の文字列
-        StringBuilder input = new StringBuilder();
-        input.append(x);
-        input.append(this.OPERATOR_DIVIDED);
-        input.append(y);
-        // 期待値
-        String result = Float.toString(x / y);
-
-        TextView view = new TextView(this.context);
-        MainActivity activity = setupActivity(operator, input, x);
-        activity.textView = view;
-
-        activity.equals(view);
-        Assert.assertEquals(result, view.getText());
-    }
-
 
 }
